0d5d0f6b1570c42425fa58d800f02d3291ff5efb
NonthNatsu <bossanovoyagerbook@gmail.com>
2025-05-25 02:47:40 +0700

    Pre Deploy B1

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "Nonthawat Suwannachairop",
-	subtitle: "Nonthanatsu-site",
+	subtitle: "Nonthanatsu-website",
 	lang: "en", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
 	themeColor: {
 		hue: 149, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
