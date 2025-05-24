c8ca1ef88d6eabb9f29977b70b62c2a9fcf83e9e
NonthNatsu <bossanovoyagerbook@gmail.com>
2025-05-25 02:52:51 +0700

    Pre Deploy B2

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://NonthaNatsu.github.io",
-	base: "/NonthNonthaNatsu_portfolio",
+	base: "/NonthaNatsu_portfolio",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
